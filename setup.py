10ed8a52121a337a65f9c2e07ebff432557ccb63
Codie Roelf <codiebeulaine@gmail.com>
2020-12-24 09:28:42 +0200

    version bump

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open('README.rst') as fh:
 
 setup(
     name='wagtail-personalisation-molo',
-    version='3.0.2',
+    version='3.0.3',
     description='A forked version of Wagtail add-on for showing personalized content',
     author='Praekelt.org',
     author_email='dev@praekeltfoundation.org',
